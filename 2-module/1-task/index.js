237076360709e189efb8a58546598914a80c1699
Viktoriia <front3@positron-it.ru>
2021-12-21 00:03:18 +0300

    Решение задач 2 модуля

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,20 @@
 function sumSalary(salaries) {
-  // ваш код...
+
+  let salary = 0
+
+  for (let oneProp in salaries) {
+
+    const prop = salaries[oneProp]
+
+    if(propIsFiniteNumber(prop)) {
+      salary += prop
+    }
+
+  }
+
+  return salary
+}
+
+function propIsFiniteNumber(key){
+  return typeof key === 'number' && isFinite(key) && !isNaN(key)
 }
--- 2-module/2-task/index.js
+++ 2-module/2-task/index.js
@@ -1,3 +1,9 @@
 function isEmpty(obj) {
-  // ваш код...
+
+    for(let prop in obj) {
+      return false
+    }
+
+    return true
+
 }
