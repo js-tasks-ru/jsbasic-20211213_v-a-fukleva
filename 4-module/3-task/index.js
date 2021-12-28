7bfc78c6c6fd06aab9d70d0e09009630c40a68a9
Viktoriia <front3@positron-it.ru>
2021-12-28 21:10:12 +0300

    Функция highlight

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,40 @@
 function highlight(table) {
-  // ваш код...
+
+    const tableBody = table.querySelector('tbody')
+    const tableRows = tableBody.querySelectorAll('tr');
+
+    tableRows.forEach(tr => {
+
+      const statusData = tr.lastElementChild;
+      const hasDataAttr = statusData.dataset.hasOwnProperty('available');
+      const isAvaliable = statusData.dataset.available === 'true';
+
+      if(hasDataAttr) {
+        setAvaliableStatus(isAvaliable, tr)
+      } else {
+        tr.setAttribute('hidden', true)
+      }
+
+      const genderData = statusData.previousElementSibling;
+      setGenderStatus(genderData, tr);
+
+      const ageData = +genderData.previousElementSibling.textContent;
+      filterAge(ageData, tr)
+
+    })
+
+    return table
+
+}
+
+function setAvaliableStatus(isAvaliable, tableRow) {
+  return isAvaliable ? tableRow.classList.add('available') : tableRow.classList.add('unavailable')
+}
+
+function setGenderStatus(gender, tableRow) {
+  return gender.textContent === 'm' ? tableRow.classList.add('male') : tableRow.classList.add('female');
+}
+
+function filterAge(age, tableRow) {
+  return age < 18 ? tableRow.style.textDecoration = "line-through" : null
 }
