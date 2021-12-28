a2c60865b2e4102ce4018e4086401a9bc8e8aadf
Viktoriia <front3@positron-it.ru>
2021-12-28 21:27:52 +0300

    Список друзей

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,13 @@
+const d = document;
+
 function makeFriendsList(friends) {
-  // ваш код...
+  const friendsList = d.createElement('ul');
+
+  friends.forEach(friend => {
+    const oneFriend = d.createElement('li');
+    oneFriend.textContent = friend.firstName + ' ' + friend.lastName;
+    friendsList.appendChild(oneFriend)
+  })
+
+  return friendsList
 }
